09b63aa77660f59d2ffd5d8a1bbd5357d725eab8
jwahn <a.talking.fat.cat@gmail.com>
2021-09-07 15:24:43 +0900

    Posts 엔티티 생성

---
+++ src/main/java/com/koscom/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.koscom.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 디폴트 생성자, 필수로 입력
+@Entity
+public class Posts {
+    @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 채번 방식
+    private Long id; // pk (auto increment, bigint)
+
+    @Column(length = 500, nullable = false) //varchar(500), not null
+    private String title;
+
+    @Column(length = 2000, nullable = false) //varchar(2000), not null
+    private String content;
+
+    private String author; // @Column 이 없어도 자동으로 컬럼 선언은 됨. String일 경우 자동으로 varchar(255), nullable=true 로 설정됨
+
+    @Builder //lombok의 builder. 어느 자리에 어떤 값이 들어갈 건지를 명확하게 표현할 수 있어서 더욱 좋다.
+    public Posts(String title, String content, String author) { //id가 없고 나머지 필드만 있는 생성자
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    //Q. builder는 무엇인가?
+    //Q. setter는 왜 없는가? 최신 Trend 에서는 setter를 쓰지 말자 쪽으로 가고 있다고 함. setter 없이 값을 어떻게 바꾸지??
+
+
+}
